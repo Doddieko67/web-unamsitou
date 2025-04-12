649a299efcf82f81720e57e7af0ae6da9f18dfb5
Doddieko67 <hectorfidelcastro67@gmail.com>
2025-04-12 16:10:11 -0700

    routers

--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router";
 import Home from "../pages/home";
 import { Main } from "../pages/Inicio";
+import { Settings } from "../pages/Settings";
+import { History } from "../pages/History";
 
 export function MyRoutes() {
   return (
@@ -8,6 +10,8 @@ export function MyRoutes() {
       <Route path="/" element={<Home />} />
       <Route path="*" element={<div>Not found</div>} />
       <Route path="/inicio" element={<Main></Main>} />
+      <Route path="/configuracion" element={<Settings />} />
+      <Route path="/historial" element={<History />} />
     </Routes>
   );
 }
