dd8548c859f8e79d99c3c703c1d7f5cf3d93a6e6
Doddieko67 <hectorfidelcastro67@gmail.com>
2025-04-12 11:36:32 -0600

    primero

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { NavBar } from './components/Navbar'
+import { MyRoutes } from './routers/routes'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <MyRoutes />
+    </>
+  )
+}
+
+export default App
