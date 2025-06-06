37db5b4bb1eeb3553e6bba564e8629b6536a3fe9
Doddieko67 <hectorfidelcastro67@gmail.com>
2025-06-05 19:37:29 -0700

    nada importante porq es de backend

--- frontend/src/url_backend.tsx
+++ frontend/src/url_backend.tsx
@@ -1,2 +1,2 @@
-export const url_backend = "https://ample-radiance-production.up.railway.app";
+export const url_backend = "https://server.vikdev.dev";
 // export const url_backend = "localhost:3000";
